b63a5a8baf739af187b5c1e4b37de96a56045ef1
talhadrz <talhadurmaz36@gmail.com>
2025-03-15 00:59:06 +0300

    Ders 18 Sonu

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+{
+    public class GetCastByIdQueryHandler : IRequestHandler<GetCastByIdQuery, GetCastByIdQueryResult>
+    {
+        private readonly MoviContext _context;
+
+        public GetCastByIdQueryHandler(MoviContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetCastByIdQueryResult> Handle(GetCastByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.casts.FindAsync(request.CastId);
+            return new GetCastByIdQueryResult
+            {
+                Biography = values.Biography,
+                CastId = values.CastId,
+                ImageUrl = values.ImageUrl,
+                Name = values.Name,
+                Overview = values.Overview,
+                Surname = values.Surname,
+                Title = values.Title
+            };
+
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/RemoveCastCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+{
+    public class RemoveCastCommandHandler : IRequestHandler<RemoveCastCommand>
+    {
+        private readonly MoviContext _context;
+
+        public RemoveCastCommandHandler(MoviContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(RemoveCastCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _context.casts.FindAsync(request.CastId);
+            _context.casts.Remove(values);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
